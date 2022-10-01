67a71dc17034861998179a78ec3b7db2bbf71710
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-10-01 18:01:04 +0700

    refactor: replace temp with query

--- app.ts
+++ app.ts
@@ -3,16 +3,17 @@ import { Invoice, Play, Performance } from './domain-types'
 export function statement(invoice: Invoice, plays: Record<string, Play>) {
 	let totalAmount = 0
 	let volumeCredits = 0
+  const playFor = (aPerformance: Performance) => plays[aPerformance.playID]
 
-	let result = `Statement for ${invoice.customer}\n`
-	const format = new Intl.NumberFormat('en-US', {
+  let result = `Statement for ${invoice.customer}\n`
+  const format = new Intl.NumberFormat('en-US', {
 		style: 'currency',
 		currency: 'USD',
 		minimumFractionDigits: 2,
-	}).format
+  }).format
 
-	for (let perf of invoice.performances) {
-		const play = plays[perf.playID]
+  for (let perf of invoice.performances) {
+		const play = playFor(perf)
 		let thisAmount = amountFor(perf, play)
 
 		// Добавление бонусов
@@ -26,32 +27,32 @@ export function statement(invoice: Invoice, plays: Record<string, Play>) {
 		result += ` ${play.name}: ${format(thisAmount / 1000)}`
 		result += ` (${perf.audience} seats)\n`
 		totalAmount += thisAmount
-	}
+  }
 	result += `Amount owed is ${format(totalAmount / 100)}\n`
 	result += `You earned ${volumeCredits} credits\n`
 	return result
 }
 
-function amountFor(perf: Performance, play: Play): number {
+function amountFor(aPerformance: Performance, play: Play): number {
 	let result = 0
 
 	switch (play.type) {
 		case 'tragedy':
 			result = 40_000
 
-			if (perf.audience > 30) {
-				result += 1000 * (perf.audience - 30)
+			if (aPerformance.audience > 30) {
+				result += 1000 * (aPerformance.audience - 30)
 			}
 
 			break
 		case 'comedy':
 			result = 30_000
 
-			if (perf.audience > 20) {
-				result += 10_000 + 500 * (perf.audience - 20)
+			if (aPerformance.audience > 20) {
+				result += 10_000 + 500 * (aPerformance.audience - 20)
 			}
 
-			result += 300 * perf.audience
+			result += 300 * aPerformance.audience
 			break
 
 		default:
