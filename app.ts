030d0cd958630ce0865bd83fe4a8edd3a47876f7
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-10-01 20:12:42 +0700

    refactor: enrich performances with play type

--- app.ts
+++ app.ts
@@ -1,41 +1,56 @@
 import { Invoice, Play, Performance } from './domain-types'
 
+
+type PerformanceWithPlay = Performance & { play: Play }
+
 export function statement(invoice: Invoice, plays: Record<string, Play>) {
 	const statementData = {
-		...invoice,
+		customer: invoice.customer,
+		performances: invoice.performances.map(enrichPerformance),
 	}
 	return renderPlainText(statementData, plays)
+
+	function enrichPerformance<T extends Performance>(
+		aPerformance: T
+	): T & { play: Play } {
+		return Object.assign({ play: playFor(aPerformance) }, aPerformance)
+	}
+	function playFor(aPerformance: Performance) {
+		return plays[aPerformance.playID]
+	}
 }
 
-function renderPlainText(data: Invoice, plays: Record<string, Play>): string {
+function renderPlainText(
+	data: {
+		customer: string
+		performances: Array<PerformanceWithPlay>
+	},
+	plays: Record<string, Play>
+): string {
 	let result = `Statement for ${data.customer}\n`
 	for (let perf of data.performances) {
 		// Вывод строки счета
-		result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)}`
+		result += ` ${perf.play.name}: ${usd(amountFor(perf) / 100)}`
 		result += ` (${perf.audience} seats)\n`
 	}
 	result += `Amount owed is ${usd(totalAmount())}\n`
 	result += `You earned ${totalVolumeCredits()} credits\n`
 	return result
 
-	function playFor(aPerformance: Performance) {
-		return plays[aPerformance.playID]
-	}
-
-	function volumeCreditsFor(aPerformance: Performance): number {
+	function volumeCreditsFor(aPerformance: PerformanceWithPlay): number {
 		let result = 0
 		result += Math.max(aPerformance.audience - 30, 0)
 
-		if (playFor(aPerformance).type == 'comedy')
+		if (aPerformance.play.type == 'comedy')
 			result += Math.floor(aPerformance.audience / 5)
 
 		return result
 	}
 
-	function amountFor(aPerformance: Performance): number {
+	function amountFor(aPerformance: PerformanceWithPlay): number {
 		let result = 0
 
-		switch (playFor(aPerformance).type) {
+		switch (aPerformance.play.type) {
 			case 'tragedy':
 				result = 40_000
 
@@ -55,7 +70,7 @@ function renderPlainText(data: Invoice, plays: Record<string, Play>): string {
 				break
 
 			default:
-				throw new Error(`unknown type: ${playFor(aPerformance).type}`)
+				throw new Error(`unknown type: ${aPerformance.play.type}`)
 		}
 		return result
 	}
