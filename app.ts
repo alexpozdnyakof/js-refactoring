c01ca85ed252f6214dd7392aec0af74516faef34
alexpozdnyakof <alexpozdnyakof@gmail.com>
2022-10-01 20:46:37 +0700

    feat: add renderHtml function

--- app.ts
+++ app.ts
@@ -5,6 +5,14 @@ export function statement(invoice: Invoice, plays: Record<string, Play>) {
 	return renderPlainText(createStatementData(invoice, plays))
 }
 
+function usd(aNumber: number): string {
+	return new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
+		minimumFractionDigits: 2,
+	}).format(aNumber / 100)
+}
+
 function renderPlainText(data: StatementData): string {
 	let result = `Statement for ${data.customer}\n`
 
@@ -17,12 +25,23 @@ function renderPlainText(data: StatementData): string {
 	result += `Amount owed is ${usd(data.totalAmount)}\n`
 	result += `You earned ${data.totalVolumeCredits} credits\n`
 	return result
+}
 
-	function usd(aNumber: number): string {
-		return new Intl.NumberFormat('en-US', {
-			style: 'currency',
-			currency: 'USD',
-			minimumFractionDigits: 2,
-		}).format(aNumber / 100)
+
+
+function renderHtml(data: StatementData): string {
+	let result = `<h1>Statement for ${data.customer}</h1>\n`
+	result += '<table>/n'
+	result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>'
+	for (let perf of data.performances) {
+		result += ` <tr><td>${perf.play.name}</td>`
+		result += `<td>${perf.audience} seats</td>`
+		result += `<td>${usd(perf.amount)}</td>`
 	}
+	result += '</table>\n'
+	result += `<p>Amount owed is: `
+	result += `<em>${usd(data.totalAmount)}</em></p>\n `
+	result += `<p>You earned <em>${data.totalVolumeCredits}`
+	result += `</em> credits</p>\n`
+	return result
 }
